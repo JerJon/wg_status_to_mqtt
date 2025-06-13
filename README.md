438c64b5e603ee0c8be96b04eaf2fe23033c509d
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2025-06-13 10:38:05 +0100

    Add some initial code

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ https://hub.docker.com/r/iainbullock/wg_status_to_mqtt
 
 For my personal use, not publically supported (unless there's enough interest)
 
-## References 
+## References / Credits
 Various ideas and code snippets have been derived from these projects, many thanks for sharing:
+ - 
  - Wireguard CLI to MQTT Bash Script - For getting Wireguard information in to Home Assistant MQTT sensors https://gist.github.com/mehstg/466045bbd0316d7be0a196c1a049477e (Paul Braham https://gist.github.com/mehstg)
  - A fork of the above https://gist.github.com/kafeg/51689dcad1b175481d7586d36a2b5af8 (Vitaly https://gist.github.com/kafeg)
 
--- app/start.sh
+++ app/start.sh
@@ -1,1 +1,32 @@
-#!/bin/sh
+#!/bin/ash
+
+# Load subroutines
+. /app/subroutines.sh
+
+# Export environment setting defaults if not defined
+export MQTT_IP=${MQTT_IP:-localhost}
+export MQTT_USERNAME=${MQTT_USERNAME:-}
+export MQTT_PASSWORD=${MQTT_PASSWORD:-}
+
+# Main Loop
+# Based on credits:
+# https://gist.github.com/mehstg/466045bbd0316d7be0a196c1a049477e
+# https://gist.github.com/kafeg/51689dcad1b175481d7586d36a2b5af8
+while IFS= read -r RESULT
+do
+    WG=$(wg)
+    IP=$(echo $WG | grep -A 4 $RESULT | grep 'endpoint' | sed s'/endpoint://' | sed -e 's/^[[:space:]]*//')
+    LASTSEEN=$(echo $WG | grep -A 4 $RESULT | grep 'latest' | sed s'/latest handshake://' | sed -e 's/^[[:space:]]*//')
+    DATA=$(echo $WG | grep -A 4 $RESULT | grep 'transfer' | sed s'/transfer://' | sed -e 's/^[[:space:]]*//')
+    PEER_ID_CLEAN=$(echo $RESULT | tr -d '+#/=,')
+
+    FINAL='{"Client":"'"$PEER_ID_CLEAN"'","IP":"'"$IP"'","Last Seen":"'"$LASTSEEN"'","Data":"'"$DATA"'"}'
+
+    echo WG $WG
+    echo IP $IP
+    echo LASTSEEN $LASTSEEN
+    echo DATA $DATA
+    echo PEER_ID_CLEAN $PEER_ID_CLEAN
+    echo FINAL $FINAL
+
+done < <(wg | grep peer: | cut -d" " -f2)
