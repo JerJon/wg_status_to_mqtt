6334e39a91260f64a9bd173cec4ea270d043480b
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2025-06-14 07:02:17 +0100

    Use wg dump option

--- app/start.sh
+++ app/start.sh
@@ -12,21 +12,19 @@ export MQTT_PASSWORD=${MQTT_PASSWORD:-}
 # Based on credits:
 # https://gist.github.com/mehstg/466045bbd0316d7be0a196c1a049477e
 # https://gist.github.com/kafeg/51689dcad1b175481d7586d36a2b5af8
-while IFS= read -r RESULT
-do
-    WG=$(wg)
-    IP=$(echo $WG | grep -A 4 $RESULT | grep 'endpoint' | sed s'/endpoint://' | sed -e 's/^[[:space:]]*//')
-    LASTSEEN=$(echo $WG | grep -A 4 $RESULT | grep 'latest' | sed s'/latest handshake://' | sed -e 's/^[[:space:]]*//')
-    DATA=$(echo $WG | grep -A 4 $RESULT | grep 'transfer' | sed s'/transfer://' | sed -e 's/^[[:space:]]*//')
-    PEER_ID_CLEAN=$(echo $RESULT | tr -d '+#/=,')
+while IFS= read -r RESULT; do
+  public_key_hash=$(echo $RESULT | awk '{print $2}' | md5sum | cut -d ' ' -f1)
+  endpoint_ip=$(echo $RESULT | awk '{print $4}' | cut -d: -f1)
+  allowed_ips=$(echo $RESULT | awk '{print $5}')
+  latest_handshake=$(echo $RESULT | awk '{print $6}')
+  transfer_rx=$(echo $RESULT | awk '{print $7}')
+  transfer_tx=$(echo $RESULT | awk '{print $8}')
 
-    FINAL='{"Client":"'"$PEER_ID_CLEAN"'","IP":"'"$IP"'","Last Seen":"'"$LASTSEEN"'","Data":"'"$DATA"'"}'
+  echo public_key_hash $public_key_hash
+  echo endpoint_ip $endpoint_ip
+  echo allowed_ips $allowed_ips
+  echo latest_handshake $latest_handshake
+  echo transfer_rx $transfer_rx
+  echo transfer_tx $transfer_tx
 
-    echo WG $WG
-    echo IP $IP
-    echo LASTSEEN $LASTSEEN
-    echo DATA $DATA
-    echo PEER_ID_CLEAN $PEER_ID_CLEAN
-    echo FINAL $FINAL
-
-done < <(wg | grep peer: | cut -d" " -f2)
+done < <(wg show all dump | awk '{if (NF==9) print $0};')
